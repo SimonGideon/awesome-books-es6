51e35b52b997e84b01936b77a89e4235ce4434a0
SimonGideon <simongideon918@gmail.com>
2022-10-31 23:12:03 +0300

    Awesome book modules

---
+++ index.js
@@ -0,0 +1,9 @@
+import { addBook } from './modules/addBook.js';
+import { displayList } from './modules/displayList.js';
+import { contactUs } from './modules/contactUs.js';
+import time from './modules/time.js';
+
+addBook();
+displayList();
+contactUs();
+time();
